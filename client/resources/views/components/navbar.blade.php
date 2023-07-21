afa7be52289174c0e029d242bd040af3496daf96
Martin <meatole.cs@gmail.com>
2023-07-21 18:59:35 +0800

    Add: Frontend

---
+++ client/resources/views/components/navbar.blade.php
@@ -0,0 +1,5 @@
+<div class="container p-50">
+    <div class="p-50">
+        <p>Spotify</p>
+    </div>
+</div>
---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+  ],
+  theme: {
+    container: {
+      center: 'true',
+      padding: '2rem',
+    }
+  },
+  plugins: [],
+}
+
