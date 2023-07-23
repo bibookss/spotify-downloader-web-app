69c7c1e01a337ca3a408d9c2604d0f40bf0ea878
NoelEmaas <nemaas@gbox.adnu.edu.ph>
2023-07-23 20:38:54 +0800

    feat: added auth check middleware

--- client/app/Http/Kernel.php
+++ client/app/Http/Kernel.php
@@ -52,7 +52,8 @@ class Kernel extends HttpKernel
      *
      * @var array<string, class-string|string>
      */
-    protected $middlewareAliases = [
+    protected $middlewareAliases = [  
+        'isLoggedIn' => \App\Http\Middleware\AuthCheck::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
