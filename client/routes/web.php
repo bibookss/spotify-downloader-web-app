811613bc6a2b604217a782df8b343ece99a89991
NoelEmaas <nemaas@gbox.adnu.edu.ph>
2023-07-23 20:39:50 +0800

    feat: assigned auth check middlware to routes

--- client/routes/web.php
+++ client/routes/web.php
@@ -24,7 +24,7 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->name('dashboard');
+})->middleware('isLoggedIn')->name('dashboard');
 
 
 // Spotify
