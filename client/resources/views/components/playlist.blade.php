8c6a9f9ca06958890dfee21bdd212b8e4145b1c6
Martin <meatole.cs@gmail.com>
2023-07-27 00:44:56 +0800

    fix: playlist structure

--- client/resources/views/components/playlist.blade.php
+++ client/resources/views/components/playlist.blade.php
@@ -6,13 +6,11 @@
                 <a href=" {{ route('spotify.playlist', ['id' => $playlist['id']]) }} "
                     class="flex bg-spotifyCard card-animation rounded-sm items-center drop-shadow-xl">
                     <!-- Playlist Image -->
-                    <img src="{{ $playlist['image'] }}" alt="{{ $playlist['name'] }}" class="sm:w-20 sm:h-20 w-12 h-12 rounded-sm">
+                    <img src="{{ $playlist['image'] }}" alt="{{ $playlist['name'] }}" class="w-20 h-auto rounded-sm">
 
                     {{-- Playlist title --}}
-                    <a href="{{ route('spotify.playlist', ['id' =>  $playlist['id']]) }}" class="text-white sm:text-base text-xs sm:font-bold font-medium ml-2">
-                        {{ $playlist['name'] }}
-                    </a>
-                </div>
+                    <p class="text-white font-bold indent-10"> {{ $playlist['name'] }} </p>
+                </a>
             @endforeach
         @endif
     </div>
