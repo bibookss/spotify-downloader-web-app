93aa2094261c70af4fc37d47be625d69950ac275
Martin <meatole.cs@gmail.com>
2023-07-27 16:29:31 +0800

    fix: hide search bar when user isn't loggedin. style: changed playlist background

--- client/resources/views/components/navbar.blade.php
+++ client/resources/views/components/navbar.blade.php
@@ -1,102 +1,121 @@
-
 <nav class="spotify-navbar top-0 fixed w-screen z-20 spotify-navbar" id="navbar">
     <div class=" flex flex-wrap items-center justify-between mx-auto py-5 sm:px-9 px-4">
-    <a href="{{route('dashboard')}}" class="flex items-center">
-        <img src="{{ asset('assets/logo/spot-logo.png') }}" class="sm:block hidden h-8 w-auto" alt="Logo">
-        <img src="{{ asset('assets/logo/logo.png') }}" class="sm:hidden block h-8 w-auto" alt="Logo">
-    </a>
-    <div class="flex md:order-2">
-      <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" class="md:hidden text-gray-300 rounded-lg text-sm p-2.5 mr-1" >
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-        </svg>
-        <span class="sr-only">Search</span>
-      </button>
-      <div class="relative hidden md:block">
-        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-          <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-          </svg>
-          <span class="sr-only">Search icon</span>
+
+        {{-- Logo --}}
+        <a href="{{ route('dashboard') }}" class="flex items-center">
+            <img src="{{ asset('assets/logo/spot-logo.png') }}" class="sm:block hidden h-8 w-auto" alt="Logo">
+            <img src="{{ asset('assets/logo/logo.png') }}" class="sm:hidden block h-8 w-auto" alt="Logo">
+        </a>
+        <div class="flex md:order-2">
+
+
+            @if (session()->has('spotifyUser') && session('spotifyUser')['image'])
+                {{-- Nav searchbar --}}
+                <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search"
+                    aria-expanded="false" class="md:hidden text-gray-300 rounded-lg text-sm p-2.5 mr-1">
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                    </svg>
+                    <span class="sr-only">Search</span>
+                </button>
+                <div class="relative hidden md:block">
+                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                        </svg>
+                        <span class="sr-only">Search icon</span>
+                    </div>
+                    <input type="text" id="search-navbar"
+                        class="block w-full p-2 pl-10 text-sm text-white border border-[#888888] rounded-lg bg-[#312828] focus:ring-gray-300 focus:border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Search...">
+                </div>
+
+                {{-- User's profile picture --}}
+                <div class="ml-5 md:mr-3 content-center items-center">
+                    <img src="{{ session('spotifyUser')['image'] }}" id="avatarButton" type="button"
+                        data-dropdown-toggle="userDropdown" data-dropdown-placement="bottom-start"
+                        class="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500 cursor-pointer"
+                        alt="User Image">
+
+                    {{-- Dropdown menu --}}
+                    <div id="userDropdown"
+                        class="z-10 hidden bg-spotifyDark divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                        <div class="px-4 py-3 text-sm  text-white">
+                            <div>{{ session('spotifyUser')['name'] }}</div>
+                            <div class="font-medium truncate"> {{ session('spotifyUser')['email'] }} </div>
+                        </div>
+                        <ul class="py-2 text-sm text-white dark:text-gray-200" aria-labelledby="avatarButton">
+                            <li>
+                                <a href="#"
+                                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 hover:text-black">Account</a>
+                            </li>
+                            <li>
+                                <a href="#"
+                                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 hover:text-black">Settings</a>
+                            </li>
+                            <li>
+                                <a href="#"
+                                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 hover:text-black">Profile</a>
+                            </li>
+                        </ul>
+
+                        {{-- Logout button --}}
+                        <div class="">
+                            <form action="{{ route('spotify.logout') }}" method="POST">
+                                @csrf
+                                <button type="submit"
+                                    class="block px-4 py-2 text-sm text-white hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 hover:text-black"">Logout</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            @endif
         </div>
-        <input type="text" id="search-navbar" class="block w-full p-2 pl-10 text-sm text-white border border-[#888888] rounded-lg bg-[#312828] focus:ring-gray-300 focus:border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search...">
-      </div>
-      {{-- User's profile picture --}}
-      @if (session()->has('spotifyUser') && session('spotifyUser')['image'])
-        <div class="ml-5 md:mr-3 content-center items-center">
-            <img src="{{ session('spotifyUser')['image'] }}" id="avatarButton" type="button"
-                data-dropdown-toggle="userDropdown" data-dropdown-placement="bottom-start"
-                class="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500 cursor-pointer"
-                alt="User Image">
 
-            <!-- Dropdown menu -->
-            <div id="userDropdown"
-                class="z-10 hidden bg-spotifyDark divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-                <div class="px-4 py-3 text-sm  text-white">
-                    <div>{{ session('spotifyUser')['name'] }}</div>
-                    <div class="font-medium truncate"> {{ session('spotifyUser')['email'] }} </div>
+        {{-- @Noel, ano ni? --}}
+        <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
+            <div class="relative mt-3 md:hidden">
+                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                    </svg>
                 </div>
-                <ul class="py-2 text-sm text-white dark:text-gray-200" aria-labelledby="avatarButton">
-                    <li>
-                        <a href="#"
-                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 hover:text-black">Account</a>
-                    </li>
-                    <li>
-                        <a href="#"
-                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 hover:text-black">Settings</a>
-                    </li>
-                    <li>
-                        <a href="#"
-                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 hover:text-black">Profile</a>
-                    </li>
-                </ul>
-
-                {{-- Logout button --}}
-                <div class="">
-                    <form action="{{ route('spotify.logout') }}" method="POST">
-                        @csrf
-                        <button type="submit"
-                            class="block px-4 py-2 text-sm text-white hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 hover:text-black"">Logout</button>
-                    </form>
-                </div>
+                <input type="text" id="search-navbar"
+                    class="block w-full p-2 pl-10 text-sm text-white border border-[#888888] bg-[#312828] rounded-lg focus:ring-gray-300 focus:border-gray-300"
+                    placeholder="Search...">
             </div>
         </div>
-      @endif
-    </div>
-      <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
-        <div class="relative mt-3 md:hidden">
-          <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-            </svg>
-          </div>
-          <input type="text" id="search-navbar" class="block w-full p-2 pl-10 text-sm text-white border border-[#888888] bg-[#312828] rounded-lg focus:ring-gray-300 focus:border-gray-300" placeholder="Search...">
-        </div>
-      </div>
     </div>
 </nav>
-  
-<script>
-  $(window).scroll(function() {
-      var scroll = $(window).scrollTop();
-      var isAuthPage = $('body').hasClass('');
 
-      if (scroll > 0) {
-          $('.spotify-navbar').addClass('spotify-navbar-scrolled');
-      } else {
-          $('.spotify-navbar').removeClass('spotify-navbar-scrolled');
-      }
-  });
+<script>
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        var isAuthPage = $('body').hasClass('');
+
+        if (scroll > 0) {
+            $('.spotify-navbar').addClass('spotify-navbar-scrolled');
+        } else {
+            $('.spotify-navbar').removeClass('spotify-navbar-scrolled');
+        }
+    });
 </script>
 
 <style scoped>
-  .spotify-navbar {
-      background-color: transparent;
-  }
+    .spotify-navbar {
+        background-color: transparent;
+    }
 
-  .spotify-navbar-scrolled {
-      transition: background-color 0.3s ease-in-out;
-      backdrop-filter: blur(8px); /* Add blur effect */
-      background-color: rgba(0, 0, 0, 0.6); /
-  }
+    .spotify-navbar-scrolled {
+        transition: background-color 0.3s ease-in-out;
+        backdrop-filter: blur(8px);
+        /* Add blur effect */
+        background-color: rgba(0, 0, 0, 0.6);/
+    }
 </style>
--- client/resources/views/play-list-page.blade.php
+++ client/resources/views/play-list-page.blade.php
@@ -3,27 +3,33 @@
 @section('content')
 
     <body>
-        <div class="container sm:px-9 px-4 py-9 flex sm:flex-row flex-col gap-x-8">	
-            <img class="md:w-[250px] md:h-[250px] w-[200px] h-[200px] sm:m-0 m-auto" src={{$playListData['image']}} alt="playlist-image">	
-            <div class="container flex flex-col justify-end">	
-                <p class="text-white sm:text-sm text-xs font-medium sm:pb-3 sm:pt-0 pt-6">Playlist</p>	
-                <h1 class="text-white font-bold md:text-7xl sm:text-5xl text-xl sm:pb-10 pb-2">{{ $playListData['name'] }}</h1>	
-                <div class="container flex md:flex-row md:items-center md:justify-normal flex-col justify-center gap-x-2 gap-y-2 sm:pt-0 pt-5">	
-                    <div class="flex flex-row items-center">	
-                        <img class="rounded-full w-8 h-8 mr-2" src="{{ session('spotifyUser')['image'] }}" alt="owner-image">	
-                        <p class="text-white font-semibold text-sm ">{{ $playListData['owner'] }}  <p class="md:block hidden">•</p></p>	
-                    </div>	
-                    <div class="flex flex-row gap-x-2">	
-                        <p class="text-white sm:text-sm text-xs font-normal">	
-                            {{ $playListData['num_tracks'] }} song{{ $playListData['num_tracks'] > 1 ? 's' : '' }},	
-                        </p>	
-                        <p class="text-white sm:text-sm text-xs font-normal">{{ $playListData['duration'] }}</p>	
-                    </div>	
-                </div>	
-            </div>	
+        <div class="container sm:px-9 px-4 py-9 flex sm:flex-row flex-col gap-x-8">
+            <img class="md:w-[250px] md:h-[250px] w-[200px] h-[200px] sm:m-0 m-auto" src={{ $playListData['image'] }}
+                alt="playlist-image">
+            <div class="container flex flex-col justify-end">
+                <p class="text-white sm:text-sm text-xs font-medium sm:pb-3 sm:pt-0 pt-6">Playlist</p>
+                <h1 class="text-white font-bold md:text-7xl sm:text-5xl text-xl sm:pb-10 pb-2">{{ $playListData['name'] }}
+                </h1>
+                <div
+                    class="container flex md:flex-row md:items-center md:justify-normal flex-col justify-center gap-x-2 gap-y-2 sm:pt-0 pt-5">
+                    <div class="flex flex-row items-center">
+                        <img class="rounded-full w-8 h-8 mr-2" src="{{ session('spotifyUser')['image'] }}" alt="owner-image">
+                        <p class="text-white font-semibold text-sm ">{{ $playListData['owner'] }}
+                        <p class="md:block hidden">•</p>
+                        </p>
+                    </div>
+                    <div class="flex flex-row gap-x-2">
+                        <p class="text-white sm:text-sm text-xs font-normal">
+                            {{ $playListData['num_tracks'] }} song{{ $playListData['num_tracks'] > 1 ? 's' : '' }},
+                        </p>
+                        <p class="text-white sm:text-sm text-xs font-normal">{{ $playListData['duration'] }}</p>
+                    </div>
+                </div>
+            </div>
         </div>
 
-        <div class="container sm:px-9 px-4 pb-8">
+        <div class=" sm:px-9 px-4 pb-8">
+            {{-- Download button --}}
             <form action="{{ route('download.playlist.server') }}" method="POST">
                 @csrf
                 <input type="hidden" name="id" value="{{ $playListData['id'] }}">
@@ -31,20 +37,26 @@
                     <x-feathericon-download style="color: black" />
                 </button>
             </form>
+
+            {{-- Download progress bar --}}
+            <div class="flex justify-between">
+                <span class="text-base font-medium text-spotifyGreen dark:text-white">Downloading your playlist...</span>
+                <span class="text-sm font-medium text-spotifyGreen dark:text-white">45%</span>
+            </div>
+            <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                <div class="bg-spotifyGreen h-2.5 rounded-full" style="width: 45%"></div>
+            </div>
         </div>
 
         <div class="sm:px-9 px-4">
             <table class="table w-full">
                 <thead class="sticky top-[60px]">
-                    <tr class="border-b border-[#A2A2A2] sticky top-[70px] bg-[#2C0A0C]">
+                    <tr class="border-b border-[#A2A2A2] sticky top-[70px] bg-[#121212]">
                         <th style="width: 4%" class="text-center font-normal py-4">#</th>
                         <th style="width: 36%" class="text-left font-normal py-4">Title</th>
-                        <th style="width: 35%"
-                            class="text-left font-normal md:table-cell hidden py-4">Album</th>
-                        <th style="width: 10%"
-                            class="text-left font-normal xl:table-cell hidden py-4">Date added</th>
-                        <th style="width: 10%"
-                            class="text-left font-normal lg:table-cell hidden py-4">Duration</th>
+                        <th style="width: 35%" class="text-left font-normal md:table-cell hidden py-4">Album</th>
+                        <th style="width: 10%" class="text-left font-normal xl:table-cell hidden py-4">Date added</th>
+                        <th style="width: 10%" class="text-left font-normal lg:table-cell hidden py-4">Duration</th>
                         <th style="width: 5%">
                             <x-feathericon-download class="h-7 m-auto" />
                         </th>
@@ -89,6 +101,6 @@
     body {
         color: white;
         padding-top: 6rem;
-        background: linear-gradient(180deg, #380609 0%, #230D0E 100%) fixed;
+        background: #121212;
     }
 </style>
