5be6ed3c1ca6d76ca5265bf1159babf13aa8c010
Martin <meatole.cs@gmail.com>
2023-07-30 12:17:42 +0800

    style: removed the spotify-downloader image

--- client/resources/views/components/play-list-card.blade.php
+++ client/resources/views/components/play-list-card.blade.php
@@ -1,9 +1,11 @@
 <div class="flex-shrink-0 sm:w-48 w-40 h-65 p-5 items-center bg-spotifyCard rounded-md card-animation">
     <!-- Display Playlist Image -->
-    @if($playlist['image'] !== null)
-        <img src="{{ $playlist['image'] }}" alt="{{ $playlist['name'] }}" class="sm:w-40 sm:h-40 w-32 h-32 object-cover rounded-md">
+    @if ($playlist['image'] !== null)
+        <img src="{{ $playlist['image'] }}" alt="{{ $playlist['name'] }}"
+            class="sm:w-40 sm:h-40 w-32 h-32 object-cover rounded-md">
     @else
-        <img src="{{ asset('assets/img/no_playlist_image.png') }}" alt="Placeholder" class="sm:w-40 sm:h-40 w-32 h-32 object-cover rounded-md">
+        <img src="{{ asset('assets/img/no_playlist_image.png') }}" alt="Placeholder"
+            class="sm:w-40 sm:h-40 w-32 h-32 object-cover rounded-md">
     @endif
 
     <a href="{{ route('spotify.playlist', ['id' => $playlist['id']]) }}"
@@ -11,7 +13,8 @@
     </a>
 
     @if (isset($playlist['description']))
-        <p class="text-spotifyDescription block overflow-hidden whitespace-nowrap text-overflow-ellipsis sm:text-base text-xs">
+        <p
+            class="text-spotifyDescription block overflow-hidden whitespace-nowrap text-overflow-ellipsis sm:text-base text-xs">
             {{ $playlist['description'] }}
         </p>
     @endif
